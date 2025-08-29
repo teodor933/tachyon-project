b178cfc74968c9827c75a99eec1e7a9ac8ecd8bf
teodor933 <teodorancuta1@gmail.com>
2025-08-29 18:25:10 +0100

    Initial Commit.

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("white")
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
