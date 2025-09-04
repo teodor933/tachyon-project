23306bc2af7d8781bab269f7f736bc22b1f1cbf4
teodor <teodor.ancuta.a@gmail.com>
2025-09-04 15:29:26 +0100

    Created config dictionary to alter game object values.

--- game.py
+++ game.py
@@ -8,9 +8,10 @@ class Game:
     The main Game class that acts as the core engine.
     It manages the game window, clock and the state manager.
     """
-    def __init__(self, screen_width: int, screen_height: int, title: str):
-        self.screen = pygame.display.set_mode((screen_width, screen_height))
-        pygame.display.set_caption(title)
+    def __init__(self, **kwargs):
+        self.screen = pygame.display.set_mode((kwargs.get("screen_width", 1920), kwargs.get("screen_height", 1920)))
+        pygame.display.set_caption(kwargs.get("title", "Tachyon"))
+        self.fps = kwargs.get("fps", 60)
 
         self.clock = pygame.time.Clock()
         self.running = True
@@ -20,7 +21,7 @@ class Game:
 
     def run(self):
         while self.running:
-            dt = self.clock.tick(60) / 1000 # time taken in milliseconds converted to seconds
+            dt = self.clock.tick(self.fps) / 1000 # time taken in milliseconds converted to seconds
 
             events = pygame.event.get()
 
